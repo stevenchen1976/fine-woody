ec99b99e95ef6274e399123c5f36eedcabc4f837
dingyuanjie <1093520060@qq.com>
2018-08-24 20:00:25 +0800

    Dao添加@Repository注解

--- woody-ding/src/main/java/com/woody/fine/dao/test/TestDao.java
+++ woody-ding/src/main/java/com/woody/fine/dao/test/TestDao.java
@@ -1,7 +1,9 @@
 package com.woody.fine.dao.test;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TestDao {
 
     String queryPriceByName(@Param("name") String name);
