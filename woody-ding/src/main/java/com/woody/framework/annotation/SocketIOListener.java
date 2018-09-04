e434f2e70de94195448d1fc590de21a20f5b0a1b
dingyuanjie <1093520060@qq.com>
2018-09-04 16:41:27 +0800

    长连接注解

---
+++ woody-ding/src/main/java/com/woody/framework/annotation/SocketIOListener.java
@@ -0,0 +1,13 @@
+package com.woody.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public  @interface SocketIOListener {
+
+    String group() default "common";
+}
